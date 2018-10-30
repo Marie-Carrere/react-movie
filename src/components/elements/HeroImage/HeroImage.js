90c7e52b51dc2421eeff3ec661ef1ca39c4843f6
marie-carrere <marie.carrere@icloud.com>
2018-10-30 21:03:09 +0100

    styling HeroImage

--- src/components/elements/HeroImage/HeroImage.js
+++ src/components/elements/HeroImage/HeroImage.js
@@ -3,7 +3,16 @@ import './HeroImage.css';
 
 const HeroImage = (props) => {
     return(
-        <div>
+        <div className="rmdb-heroimage"
+            style={{
+                background:
+                `linear-gradient(to bottom, rgba(0,0,0,0)
+                39%, rgba (0,0,0,0)
+                41%, rgba(0,0,0,0.65)
+                100%),
+                url('${props.image}'), #1c1c1c`
+            }}
+        >
             HeroImage
         </div>
     );
