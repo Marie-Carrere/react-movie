89e81c16bb4bff4b9e23e4907307f9440ac038ff
marie-carrere <marie.carrere@icloud.com>
2018-10-31 11:48:13 +0100

    movie thumbnails done

--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -4,7 +4,9 @@ import './MovieThumb.css';
 const MovieThumb = (props) => {
     return(
         <div>
-            MovieThumb
+            <div className="rmdb-moviethumb">
+                <img src={props.image} alt="moviethumb"/>
+            </div>
         </div>
     );
 }
