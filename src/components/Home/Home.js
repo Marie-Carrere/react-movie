4d94766401ce9f3df0dabc7f412a5160432cafd2
marie-carrere <marie.carrere@icloud.com>
2018-10-31 09:19:23 +0100

    created the searchbar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -24,6 +24,23 @@ class Home extends Component {
         this.fetchItems(endpoint);
     }
 
+    searchItems = (searchTerm) => {
+        let endpoint = '';
+        this.setState({
+            movies: [],
+            loading: true,
+            searchTerm
+        })
+
+        if(searchTerm === '') {
+            endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        } else {
+            endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query=${searchTerm}`;
+        }
+        this.fetchItems(endpoint);
+
+    }
+
     loadMoreItems = () => {
         let endpoint = '';
         this.setState({ loading: true });
@@ -60,7 +77,7 @@ class Home extends Component {
                             title={this.state.heroImage.original_title}
                             text={this.state.heroImage.overview}
                         />
-                        <SearchBar />
+                        <SearchBar callback={this.searchItems} />
                     </div> : null }
                     <FourColGrid />
                     <Spinner />
